d32f0a273a5292210a60d80fc90e6fc92eb1f563
Murphybread <nargene@gmail.com>
2023-12-13 00:25:13 +0900

    data binding

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <h4>{{ products[0]}}</h4>
+    <p>50만원</p>
+  </div>
+
+  <div>
+    <h4>{{ products[1]}}</h4>
+    <p>60만원</p>
+  </div>
+    <div>
+    <h4>{{ products[2]}}</h4>
+    <p>70만원</p>
+  </div> 
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }
+  },
+  components: {
+   
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
